1b14b3528a441b1e3e157bbf7bc047d58fdc0e83
Anthony Montoya <a.montoya1073@gmail.com>
2017-08-13 21:07:57 -0600

    Finished FilterObject

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -4,19 +4,45 @@ import React, { Component } from 'react';
 class EvenAndOdd extends Component {
     constructor() {
         super();
+
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: "",
+        };
     }
+
+     userInputUpdate(inputUpdate) {
+            this.setState ({
+                userInput: inputUpdate
+            })
+        }
+
+        updateArray(numStr) {
+            let evens = [];
+            let odds = [];
+            let numSplit = numStr.split(",");
+            for(var i = 0; i < numSplit.length; i++) {
+                if(numSplit[i] % 2 === 0) {
+                    evens.push(parseInt(numSplit[i]));
+                } else {
+                    odds.push(parseInt(numSplit[i]));
+                }
+            }
+            this.setState ({
+                evenArray: evens,
+                oddArray: odds
+            })
+        }
 
     render() {
         return (
             <div className="puzzleBox evenAndOddPB">
                 <h4>Evens and Odds</h4>
-                <input className="inputLine"></input>
-                <button className="confirmationButton"></button>
-                <span className="resultsBox"></span>
-                <span className="resultsBox"></span>
-
-
-
+                <input className="inputLine" onChange= { (e) => this.userInputUpdate(e.target.value) }></input>
+                <button className="confirmationButton" onClick= { () => this.updateArray(this.state.userInput) }>Split</button>
+                <span className="resultsBox">Even Array: { JSON.stringify(this.state.evenArray) } </span>
+                <span className="resultsBox">Odd Array: {JSON.stringify(this.state.oddArray) } </span>
             </div>
         )
     }
--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -4,11 +4,58 @@ import React, { Component } from 'react';
 class FilterObject extends Component {
     constructor() {
         super();
+
+        this.state = {
+            unfilteredArray: [
+                {
+                    name: 'Jimmy Joe',
+                    title: 'Hack0r',
+                    age: 12,
+                },
+                {
+                    name: 'Jeremy Schrader',
+                    age: 24,
+                    hairColor: 'brown'
+                },
+                {
+                    name: 'Carly Armstrong',
+                    title: 'CEO',
+                }
+            ],
+            userInput: "",
+            filteredArray: []
+        };
+    }
+
+    userInputUpdate(inputUpdate) {
+        this.setState ({
+            userInput: inputUpdate
+        })
+    }
+
+    objectFilter(obj) {
+        let filteredArray = [];
+        let unfilteredArray = this.state.unfilteredArray;
+
+        for(var i = 0; i < unfilteredArray.length; i++) {
+            if(unfilteredArray[i].hasOwnProperty(obj)) {
+                filteredArray.push(unfilteredArray[i]);
+            }
+        }
+        this.setState ({
+            filteredArray: filteredArray
+        })
     }
 
     render() {
         return (
-            <p>FilterObject Component</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className="puzzleText">Original: { JSON.stringify(this.state.unfilteredArray) }</span>
+                <input className="inputLine" onChange= { (e) => this.userInputUpdate(e.target.value) }></input>
+                <button className="confirmationButton" onClick= { () => this.objectFilter(this.state.userInput) }>Filter</button>
+                <span className="resultsBox filterObjectRB">Filtered: { JSON.stringify(this.state.filteredArray) }</span>
+            </div>
         )
     }
 }
